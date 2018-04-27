a1add7dda328c0dd72b172b8d0e7b9a266cecfee
aa.malcev2 <aa.malcev2@tensor.ru>
2018-04-27 17:12:23 +0300

    Тестирование ESM под Node.js

--- isolated.js
+++ isolated.js
@@ -3,6 +3,7 @@
 /**
  * Runs testing via Node.js
  * @param {Object} config Config
+ * @param {String} [config.moduleType='esm'] Testing module type: 'esm' - ECMAScript Module, 'amd' - Asynchronous Module Definition
  * @param {String} [config.ws=''] Path to WS core
  * @param {String} [config.resources=''] Path to resources folder
  * @param {String} [config.tests] Path to tests folder (relative to config.resources)
@@ -10,6 +11,7 @@
  */
 exports.run = function(config) {
    config = config || {};
+   config.moduleType = config.moduleType || 'esm';
    config.root = config.root || '';
    config.ws = config.ws || '';
    config.resources = config.resources || '';
