bbe7c1c32e8b2ce82027a2dcd367103c98beab19
aa.malcev2 <aa.malcev2@tensor.ru>
2018-05-22 17:07:49 +0300

    Тесты ESM модулей

--- mocha.js
+++ mocha.js
@@ -12,10 +12,12 @@ let spawn = require('child_process').spawn,
    path = require('path'),
    pathTo = require('./lib/util').pathTo,
    args = [
-//      '--experimental-modules',
-//      '--loader',
-//      './node_modules/ws-unit-testing/lib/esmLoader.mjs',
-      path.join(pathTo('mocha'), 'bin', 'mocha')
+      //'--experimental-modules',
+      //'--loader',
+      //'./node_modules/ws-unit-testing/lib/esmLoader.mjs',
+      path.join(pathTo('mocha'), 'bin', 'mocha'),
+      '--compilers',
+      'js:babel-core/register'
    ];
 
 args.push.apply(args, process.argv.slice(2));
