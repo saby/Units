f8da019e14ca57e19bce279a214ce7cece588af5
aa.malcev2 <aa.malcev2@tensor.ru>
2018-05-23 13:15:56 +0300

    Тесты ESM модулей

--- mocha.js
+++ mocha.js
@@ -5,23 +5,27 @@
 /**
  * This wrapper runs mocha in valid environment.
  * Usage:
- * node node_modules/ws-unit-testing/mocha -t 10000 -R path/to/your/test/runner.js
+ * node node_modules/ws-unit-testing/mocha[ --esm] -t 10000 -R path/to/your/test/runner.js
  */
 
 let spawn = require('child_process').spawn,
    path = require('path'),
    pathTo = require('./lib/util').pathTo,
-   args = [
-      //'--experimental-modules',
-      //'--loader',
-      //'./node_modules/ws-unit-testing/lib/esmLoader.mjs',
-      path.join(pathTo('mocha'), 'bin', 'mocha'),
-      '--compilers',
-      'js:babel-core/register'
-   ];
+   inheritedArgs = process.argv.slice(2),
+   args = [path.join(pathTo('mocha'), 'bin', 'mocha')];
 
-args.push.apply(args, process.argv.slice(2));
+let esmFlagAt = inheritedArgs.indexOf('--esm');
+if (esmFlagAt > -1) {
+   inheritedArgs.splice(esmFlagAt, 1);
+   //'--experimental-modules',
+   //'--loader',
+   //'./node_modules/ws-unit-testing/lib/esmLoader.mjs',
+   args.push('--compilers', 'js:babel-core/register');
+}
 
+args.push.apply(args, inheritedArgs);
+
+console.log('spawn', process.execPath, args);
 let proc = spawn(
    process.execPath,
    args,
