e722ee3a32ad0fc34e008dcfd42d26d8579c82d2
aa.malcev2 <aa.malcev2@tensor.ru>
2018-09-17 14:14:50 +0300

    https://online.sbis.ru/opendoc.html?guid=080da32e-fee3-497a-959c-d3bc6e94d241 Now we can run testing in any resources folder.

--- isolated.js
+++ isolated.js
@@ -6,17 +6,14 @@
  * Runs testing via Node.js
  * @param {Object} config Config
  * @param {String} [config.moduleType='esm'] Testing module type: 'esm' - ECMAScript Module, 'amd' - Asynchronous Module Definition
- * @param {String} [config.ws=''] Path to WS core
- * @param {String} [config.resources=''] Path to resources folder
- * @param {String} [config.tests] Path to tests folder (relative to config.resources)
+ * @param {String} [config.root=''] Path to the project root
+ * @param {String} [config.tests] Path to tests folder (relative to config.root)
  * @param {String} [config.reportFile=''] Path to report file
  */
 exports.run = function(config) {
    config = config || {};
    config.moduleType = config.moduleType || 'esm';
    config.root = config.root || '';
-   config.ws = config.ws || '';
-   config.resources = config.resources || '';
    config.tests = config.tests || config.resources;
    config.reportFile = config.reportFile || '';
 
