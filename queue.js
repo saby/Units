7cda3531e1ebcc8e46003c1c151dd9cc888a8635
aa.malcev2 <aa.malcev2@tensor.ru>
2019-07-08 09:39:31 +0300

    Wait for any stdout in child process https://online.sbis.ru/opendoc.html?guid=68e9d27d-b12c-46a3-a009-303ac3366577

--- queue.js
+++ queue.js
@@ -10,7 +10,7 @@ let args = process.argv.slice(2);
 let processes = [];
 let finished = [];
 
-const DELAY = 3000; // Delay between processes run
+const DELAY = 10000; // Max delay between processes run
 
 function finishEarly(index) {
    if (index === undefined) {
@@ -23,28 +23,50 @@ function finishEarly(index) {
 }
 
 function runProcess(command, args, index) {
-   args.unshift(command);
-   let proc = spawn(
-      process.execPath,
-      args,
-      {stdio: 'inherit'}
-   );
+   return new Promise(function(resolve, reject) {
+      args.unshift(command);
+      let proc = spawn(
+         process.execPath,
+         args,
+         {stdio: 'inherit'}
+      );
 
-   processes.push(proc);
+      processes.push(proc);
 
-   proc.on('exit', (code, signal) => {
-      finished.push({
-         script: command,
-         index: index,
-         code: code,
-         signal: signal
+      proc.stdout.on('data', () => {
+         resolve(proc);
+      });
+      proc.stderr.on('data', () => {
+         reject(proc);
       });
 
-      // Finish previous
-      finishEarly(index);
-   });
+      proc.on('exit', (code, signal) => {
+         finished.push({
+            script: command,
+            index: index,
+            code: code,
+            signal: signal
+         });
 
-   return proc;
+         // Finish previous
+         finishEarly(index);
+      });
+
+      setTimeout(() => {
+         resolve(proc);
+      }, DELAY);
+   });
+}
+
+function runOneByOne(scripts, scriptsArgs, index) {
+   let script = scripts[index];
+   if (!script) {
+      return;
+   }
+   let args = scriptsArgs[index] || [];
+   runProcess(path.resolve(script), args, index).then(() => {
+      runOneByOne(scripts, scriptsArgs, 1 + index);
+   });
 }
 
 // Scripts and arguments
@@ -63,13 +85,8 @@ let scripts = args.filter((item) => {
    return !isArgument;
 });
 
-// Run children through delay
-scripts.forEach((script, index) => {
-   setTimeout(() => {
-      let args = scriptsArgs[index] || [];
-      runProcess(path.resolve(script), args, index);
-   }, index * DELAY);
-});
+// Run scripts one by one
+runOneByOne(scripts, scriptsArgs, 0);
 
 // Check for max exit code for each finished child
 process.on('exit', () => {
