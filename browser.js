464cf7b533b10aa67191dde4d3407a40f0b51492
Кудрявцев И.С <is.kudryavcev@corp.tensor.ru>
2021-03-15 15:29:13 +0300

    https://online.sbis.ru/opendoc.html?guid=430d8167-c979-40df-a034-51f069d1a8fe

--- browser.js
+++ browser.js
@@ -6,12 +6,13 @@
  * @param {String} provider Name provider to use.
  * @param {Boolean} head Use headed chrome.
  */
-exports.run = function(url, report, coverageReport, provider, head) {
+exports.run = function(url, report, coverageReport, provider, head, driverPort) {
    require('./lib/browser').run({
       url: url,
       reportFile: report,
       coverageReportFile: coverageReport,
       provider: provider,
-      head: head
+      head: head,
+      driverPort: driverPort
    });
 };
