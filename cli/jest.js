340b00c4d25d4fde686c63e2a21f191a1d5d8c5e
ma.krylov <ma.krylov@tensor.ru>
2021-06-07 17:39:01 +0300

    https://online.sbis.ru/opendoc.html?guid=d98afd9e-fd4d-43d6-a5e4-794aa67d1166

--- cli/jest.js
+++ cli/jest.js
@@ -6,6 +6,7 @@
 
 const spawn = require('child_process').spawn;
 const { parseArguments } = require('../lib/jest/command');
+const runStatics = require('../lib/jest/statics');
 
 const logger = console;
 const inputArguments = process.argv.slice(2);
@@ -14,7 +15,12 @@ const jestArguments = parseArguments(inputArguments, process.env);
 
 // Максимально возможное время, которое отведено для 1 Jest-процесса.
 // Если процесс выходит за отведенное ему время, процесс принудительно завершится.
-const HARD_PROCESS_TIMEOUT = 180 * 1000;
+const HARD_PROCESS_TIMEOUT = 5 * 60 * 1000;
+
+if (jestArguments.parameters.isBrowser) {
+   // Необходимо раздать статику
+   runStatics(jestArguments.parameters.port);
+}
 
 logger.log(`[jest] Running: ${jestArguments.args.join(' ')}`);
 
