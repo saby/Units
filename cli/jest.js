92c3e72d46824698205e01ccd1e06d25012f517d
Крылов М.А <ma.krylov@tensor.ru>
2021-06-15 12:53:17 +0300

    https://online.sbis.ru/opendoc.html?guid=d98afd9e-fd4d-43d6-a5e4-794aa67d1166

---
+++ cli/jest.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+/**
+ * This wrapper runs Jest in valid environment.
+ */
+
+const spawn = require('child_process').spawn;
+const { parseArguments } = require('../lib/jest/command');
+const runStatics = require('../lib/jest/statics');
+
+const logger = console;
+const inputArguments = process.argv.slice(2);
+const jestArguments = parseArguments(inputArguments, process.env);
+
+
+// Максимально возможное время, которое отведено для 1 Jest-процесса.
+// Если процесс выходит за отведенное ему время, процесс принудительно завершится.
+const HARD_PROCESS_TIMEOUT = 5 * 60 * 1000;
+
+if (jestArguments.parameters.isBrowser) {
+   // Необходимо раздать статику
+   runStatics(jestArguments.parameters.port, jestArguments.parameters.root);
+}
+
+logger.log(`[jest] Running: ${jestArguments.args.join(' ')}`);
+
+const jestProcess = spawn(
+   process.execPath,
+   jestArguments.args,
+   jestArguments.options
+);
+
+setTimeout(() => {
+   process.kill(jestProcess.pid, 'SIGKILL');
+}, HARD_PROCESS_TIMEOUT)
+
+jestProcess.on('exit', (code, signal) => {
+   process.on('exit', function() {
+      if (signal) {
+         process.kill(process.pid, signal);
+      } else {
+         process.exit(code);
+      }
+   });
+});
+
+// Terminate children
+process.on('SIGINT', () => {
+   jestProcess.kill('SIGINT');
+   jestProcess.kill('SIGTERM');
+   process.kill(process.pid, 'SIGINT');
+});
